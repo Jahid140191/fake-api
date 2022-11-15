d817033e0706e4bf334c4437f5b3ad4f4016ceab
Jahid140191 <85978004+Jahid140191@users.noreply.github.com>
2022-11-15 12:46:42 +0600

    use fake api

---
+++ photo/photo.js
@@ -0,0 +1,10 @@
+const postDisplay=document.querySelector('#display');
+
+async function getResult(){
+  let res = await fetch('https://jsonplaceholder.typicode.com/photos');
+  let jsonData= await res.json();
+  let countData =jsonData.slice(0,10);
+  let Data = JSON.stringify(countData);
+
+    postDisplay.innerText = Data;
+}
